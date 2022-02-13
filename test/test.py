bd5df5c17740916b1931ebfc423ec6e734b189a1
alser15 <mar233@mail.ru>
2022-02-13 21:12:18 +0300

    test

--- test/test.py
+++ test/test.py
@@ -4,14 +4,8 @@ from lib.my_requests import MyRequests
 
 class TestUserPut:
 
-    def test_change_data_user_without_auth(self, create_login_delete_user):
-        response = MyRequests.put(f'/user/{create_login_delete_user.json()["user_id"]}',
-                                  data={'username':'Vanay'})
 
-        Assertions.assert_response_text(response, 'Auth token not supplied',
-                                        'Неверный текст')
-
-    def test_change_data_user_with_auth_another_user(self, create_login_delete_user):
+    def test_(self, create_login_delete_user):
         response = MyRequests.put(f'/user/2',
             data={'username': 'Vanay'},
             headers={'x-csrf-token':create_login_delete_user.headers['x-csrf-token']},
